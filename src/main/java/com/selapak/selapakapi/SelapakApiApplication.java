cdf38be67ddf32f4bf94416f3326a726db03bac5
Rais A. <nandaliyan.rais@gmail.com>
2024-05-18 11:07:04 +0700

    [feature] Initial project

---
+++ src/main/java/com/selapak/selapakapi/SelapakApiApplication.java
@@ -0,0 +1,13 @@
+package com.selapak.selapakapi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SelapakApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SelapakApiApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/selapak/selapakapi/SelapakApiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.selapak.selapakapi;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SelapakApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
