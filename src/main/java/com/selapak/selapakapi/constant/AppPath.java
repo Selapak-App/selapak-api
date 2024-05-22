ce01618fd0a377daec2bc3f7b3be4c9dfe93d604
Rais A. <nandaliyan.rais@gmail.com>
2024-05-22 12:40:17 +0700

    [improve] modify api path

--- src/main/java/com/selapak/selapakapi/constant/AppPath.java
+++ src/main/java/com/selapak/selapakapi/constant/AppPath.java
@@ -2,23 +2,21 @@ package com.selapak.selapakapi.constant;
 
 public class AppPath {
     
-    public final static String ADMIN_PATH = "/admin";
-    public final static String CUSTOMER_PATH = "/customers";
-    public final static String RENT_PERIOD_PATH = "/rent-periods";
-    public final static String LAND_OWNER_PATH = "/land-owners";
-    public final static String LAND_PRICE_PATH = "/land-prices";
-    public final static String BUSINESS_TYPE_PATH = "/business-types";
-    public final static String LAND_PATH = "/lands";
-    public final static String TRANSACTION_PATH = "/transactions";
+    public final static String CUSTOMER_PATH = "/api/customers";
+    public final static String RENT_PERIOD_PATH = "/api/rent-periods";
+    public final static String LAND_OWNER_PATH = "/api/land-owners";
+    public final static String LAND_PRICE_PATH = "/api/land-prices";
+    public final static String BUSINESS_TYPE_PATH = "/api/business-types";
+    public final static String LAND_PATH = "/api/lands";
+    public final static String TRANSACTION_PATH = "/api/transactions";
 
-    public final static String REGISTER_PATH = "/register";
-    public final static String LOGIN_PATH = "/login";
+    public final static String REGISTER_PATH = "/api/auth/register";
+    public final static String LOGIN_PATH = "/api/auth/login";
+    public final static String REGISTER_ADMIN_PATH = "/api/auth/admin/register";
+    public final static String LOGIN_ADMIN_PATH = "/api/auth/admin/login";
 
     public final static String GET_BY_ID = "/{id}";
     public final static String UPDATE_BY_ID = "/{id}";
     public final static String DELETE_BY_ID = "/{id}";
-
-    public final static String REGISTER_ADMIN_PATH = ADMIN_PATH + REGISTER_PATH;
-    public final static String LOGIN_ADMIN_PATH = ADMIN_PATH + LOGIN_PATH;
 
 }
